b6571a758e3e644ad5a6ee8cf41da38cd4c3d29e
Shuang Wu <swu18@studentmail.nait.ca>
2015-09-16 16:20:34 -0600

    Practice
    Practice for Github

---
+++ PracticeWebsite/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PracticeWebsite.Startup))]
+namespace PracticeWebsite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
